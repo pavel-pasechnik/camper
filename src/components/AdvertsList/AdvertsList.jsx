b17aba6e3c1bd09c6478aaf9379b513d8f38bae2
Pavel <Pavel.Pasechnik@gmail.com>
2024-08-10 12:37:33 +0300

    added pagination

--- src/components/AdvertsList/AdvertsList.jsx
+++ src/components/AdvertsList/AdvertsList.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import css from './AdvertsList.module.css';
@@ -6,17 +7,27 @@ import Advert from '../Advert/Advert.jsx';
 
 const AdvertsList = () => {
   const adverts = useSelector(selectFilteredAdverts);
+  const [visibleCount, setVisibleCount] = useState(5); // Количество видимых объявлений
+
+  const handleLoadMore = () => {
+    setVisibleCount(prevCount => prevCount + 5); // Увеличиваем количество отображаемых объявлений на 5
+  };
 
   return (
-    <ul>
-      {adverts.map(advert => {
-        return (
+    <div className={css.advert}>
+      <ul>
+        {adverts.slice(0, visibleCount).map(advert => (
           <li key={advert._id} className={css.item}>
             <Advert {...advert} />
           </li>
-        );
-      })}
-    </ul>
+        ))}
+      </ul>
+      {visibleCount < adverts.length && ( // Показываем кнопку, только если есть еще объявления для загрузки
+        <button className={css['load-more']} type='button' onClick={handleLoadMore}>
+          Load more
+        </button>
+      )}
+    </div>
   );
 };
 
